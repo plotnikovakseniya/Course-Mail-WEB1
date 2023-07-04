98fa5d06554d4ea92f0f73798031db81562c2611
whiterabbit <plotnikova.kse@gmail.com>
2023-07-04 20:47:47 +0300

    realization of simple tcp server and client

---
+++ SimpleTCPServer/TCPClient.py
@@ -0,0 +1,16 @@
+import socket
+req = "Hello tcp!"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('0.0.0.0', 2222))
+s.send(req)
+rsp = s.recv(1024)
+print(rsp)
+req = "one more time"
+s.send(req)
+rsp = s.recv(1024)
+print(rsp)
+req = "close"
+s.send(req)
+rsp = s.recv(1024)
+print(rsp)
+s.close()
